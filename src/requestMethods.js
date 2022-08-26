6e1531db1fd05cf9705154778fde8c9d0a8b0056
uceiifali <musiicxxx@gmail.com>
2022-08-26 07:26:25 +0200

    dsadsad

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -12,5 +12,7 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  header: { token: `Snake  ${TOKEN}` },
+  header: {
+    token: `Snake eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZmVhYjI2ZTgwOWRjNThkMGI5MDI5OCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MTQ5MTU1NywiZXhwIjoxNjYxNzUwNzU3fQ.8iDLA7MEtUWBTSrd6hg8jAu3M_lfeM5g366Nqqq1TD0`,
+  },
 });
