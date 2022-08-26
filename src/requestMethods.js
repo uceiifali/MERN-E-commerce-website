6a3a74f5750dbe7a5df02a2ad589964c4978185a
uceiifali <musiicxxx@gmail.com>
2022-08-26 07:27:30 +0200

    sss

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const BASE_URL =
   process.env.BACKEND || "https://mernecomercewebsite.herokuapp.com/api";
 
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
-  .currentUser.accessToken;
+// const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+//   .currentUser.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
