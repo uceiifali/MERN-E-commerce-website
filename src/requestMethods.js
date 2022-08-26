4740b8fca6b2d987a862607dfce57871c93bc142
uceiifali <musiicxxx@gmail.com>
2022-08-26 07:28:52 +0200

    token issue

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -3,8 +3,8 @@ import axios from "axios";
 const BASE_URL =
   process.env.BACKEND || "https://mernecomercewebsite.herokuapp.com/api";
 
-// const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
-//   .currentUser.accessToken;
+const TOKEN =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZmVhYjI2ZTgwOWRjNThkMGI5MDI5OCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MTQ5MTU1NywiZXhwIjoxNjYxNzUwNzU3fQ.8iDLA7MEtUWBTSrd6hg8jAu3M_lfeM5g366Nqqq1TD0";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
@@ -12,7 +12,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  header: {
-    token: `Snake eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZmVhYjI2ZTgwOWRjNThkMGI5MDI5OCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MTQ5MTU1NywiZXhwIjoxNjYxNzUwNzU3fQ.8iDLA7MEtUWBTSrd6hg8jAu3M_lfeM5g366Nqqq1TD0`,
-  },
+  header: { token: `Snake  ${TOKEN}` },
 });
